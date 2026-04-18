970a221c2cb6b4f6cccd23225e09178f72e325e1
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2026-04-18 13:25:18 +0600

    added new code settings for UsrController.java / controller

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,11 +1,49 @@
-// htps requests
+//// htps requests
+//package com.example.demo.controller;
+//
+//import com.example.demo.dto.UserDTO;
+//import com.example.demo.service.UserService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.*;
+//import java.util.List;
+//
+//@RestController
+//@RequestMapping("/api/users")
+//public class UserController {
+//
+//    @Autowired
+//    private UserService userService;
+//
+//    @GetMapping
+//    public ResponseEntity<List<UserDTO>> getAllUsers() {
+//        List<UserDTO> users = userService.getAllUsers();
+//        return ResponseEntity.ok(users);
+//    }
+//
+//    @GetMapping("/{id}")
+//    public ResponseEntity<UserDTO> getUserById(@PathVariable Long id) {
+//        UserDTO user = userService.getUserById(id);
+//        return ResponseEntity.ok(user);
+//    }
+//
+//    @PostMapping
+//    public ResponseEntity<UserDTO> createUser(@RequestBody UserDTO userDTO) {
+//        UserDTO createdUser = userService.createUser(userDTO);
+//        return new ResponseEntity<>(createdUser, HttpStatus.CREATED);
+//    }
+//}
+
+// ./mvnw spring-boot:run
+
+
+// new code
 package com.example.demo.controller;
 
-import com.example.demo.dto.UserDTO;
-import com.example.demo.service.UserService;
+import com.example.demo.entity.User;
+import com.example.demo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
@@ -14,28 +52,15 @@ import java.util.List;
 public class UserController {
 
     @Autowired
-    private UserService userService;
+    private UserRepository userRepository;
 
     @GetMapping
-    public ResponseEntity<List<UserDTO>> getAllUsers() {
-        List<UserDTO> users = userService.getAllUsers();
-        return ResponseEntity.ok(users);
-    }
-
-    @GetMapping("/{id}")
-    public ResponseEntity<UserDTO> getUserById(@PathVariable Long id) {
-        UserDTO user = userService.getUserById(id);
-        return ResponseEntity.ok(user);
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
     }
 
     @PostMapping
-    public ResponseEntity<UserDTO> createUser(@RequestBody UserDTO userDTO) {
-        UserDTO createdUser = userService.createUser(userDTO);
-        return new ResponseEntity<>(createdUser, HttpStatus.CREATED);
+    public User createUser(@RequestBody User user) {
+        return userRepository.save(user);
     }
 }
-
-// ./mvnw spring-boot:run
-
-
-
