bf446decb5a96ff2aeb2efbebe1f1be81152184b
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2026-04-25 11:55:37 +0600

    setting up gettrs setters with understanding

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,9 +2,6 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import org.springframework.context.ApplicationEvent;
-import org.springframework.context.event.EventListener;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,15 +12,13 @@ public class DemoApplication {
         SpringApplication.run(DemoApplication.class, args);
     }
 
-
-
     @GetMapping("/hello")
     public String sayHello() {
         return "Hello World! This is new project!";
     }
-}
 
-// business logic// diff interface and class
-// diff interface and class
-// copy all with command - ask AI about (claud.ai) - ask curiously
-//interface - working with db
+    @GetMapping("/sanjar")
+    public String sanjar() {
+        return "Hello, this is Sanjar's DB";
+    }
+}
--- src/main/java/com/example/demo/entity/UserEntity.java
+++ src/main/java/com/example/demo/entity/UserEntity.java
@@ -6,19 +6,34 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
+@Table(name = "users_entity")
 public class UserEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
+
+    @Column(name = "name", nullable = false)
     private String name;
 
-    //construction getter setter
-    public UserEntity() {
+    @Column(name = "email", unique = true)
+    private String email;
+
+    @Column(name = "telephone")
+    private String telephone;
+
+    //construction of getters and setters
+    public UserEntity(String name, String email, String telephone) {
+        this.name = name;
+        this.email = email;
+        this.telephone = telephone;
+        // id generates automatically
     }
 
-    public UserEntity(String name) {
-        this.name = name;
+    public UserEntity() {
+
     }
+
 
     //getters and setters
     public Long getId() {
@@ -47,18 +62,14 @@ public class UserEntity {
 //    private String name;
 //    private String email;
 
-//  -======  Constructors
-//    public UserEntity() {}
+//  ======  Constructors ======
 //    public UserEntity(String name, String email) {
 //        this.name = name;
 //        this.email = email;
 //    }
-//
-//    public UserEntity(String name) {
-//    }
+
 
     // ======   Getter and Setter
-
 //    public String getName() { return name; }
 //    public void setName(String name) { this.name = name; }
 //    public String getEmail() { return email; }
