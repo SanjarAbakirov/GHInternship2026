6753edcbc98e999a4d389df81dea3b33ba1e9a82
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2026-06-06 21:41:58 +0600

    added JwtUtil.java to securities

---
+++ src/main/java/com/example/demo/securities/JwtUtil.java
@@ -0,0 +1,37 @@
+package com.example.demo.securities;
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+    private final SecretKey key = Keys.hmacShaKeyFor("my-very-secret-key-my-very-secret-key".getBytes()); // минимум 32 байта
+    private final long expirationMs = 3600000; // 1 час
+
+    public String generateToken(String username) {
+        return Jwts.builder()
+                .subject(username)
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + expirationMs))
+                .signWith(key)
+                .compact();
+    }
+
+    public String getUsernameFromToken(String token) {
+        return Jwts.parser().verifyWith(key).build()
+                .parseSignedClaims(token).getPayload().getSubject();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser().verifyWith(key).build().parseSignedClaims(token);
+            return true;
+        } catch (JwtException | IllegalArgumentException e) {
+            return false;
+        }
+    }
+}
