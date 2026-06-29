886049227d7407756979783d8a28cc5a0816b8ff
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2026-06-29 21:35:37 +0600

    deleted duble UserEntity

--- src/main/java/com/example/demo/controller/ChatController.java
+++ src/main/java/com/example/demo/controller/ChatController.java
@@ -1,5 +1,4 @@
 package com.example.demo.controller;
-
 import com.example.demo.dto.ChatRequest;
 import com.example.demo.dto.ChatResponse;
 import com.example.demo.service.ChatService;
@@ -9,36 +8,18 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-//@Slf4j
 @RestController
 @RequestMapping("/api/chat")
 public class ChatController {
 
     private static final Logger log = LoggerFactory.getLogger(ChatController.class);
+
     private final ChatService chatService;
+
     public ChatController(ChatService chatService) {
         this.chatService = chatService;
     }
 
-//    private final AiService aiService;
-//    public ChatController(AiService aiService) {
-//        this.aiService = aiService;
-//    }
-
-//    @PostMapping
-//    public ResponseEntity<?> chat(@RequestBody Map<String, String> request) {
-//        String userMessage = request.get("message");
-//        log.info("Chat request: {}", userMessage);
-//        try {
-//            String reply = aiService.getChatResponse(userMessage);
-//            log.debug("AI reply: {}", reply);
-//            return ResponseEntity.ok(Map.of("reply", reply));
-//        } catch (Exception e) {
-//            log.error("Error while calling AI service", e);
-//            return ResponseEntity.internalServerError()
-//                    .body(Map.of("error", "AI service unavailable"));
-//        }
-//    }
     @PostMapping
     public ResponseEntity<ChatResponse> chat(@Valid @RequestBody ChatRequest request) {
         log.info("Received chat message: {}", request.getMessage());
@@ -47,7 +28,3 @@ public class ChatController {
         return ResponseEntity.ok(new ChatResponse(aiReply));
     }
 }
-
-//logging - need to check out logging
-// create log messages APi controllers - gpt
-// lgo-messages - когда сервер выдает инфо о себе что происходит
