4c4476f0bbfde523f59ac451a950c369478ba7bb
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2026-09-18 23:08:58 +0600

    feat: add GET /hello endpoint and configure H2 datasource

---
+++ src/main/java/com/ghInternship/GHInternship2026/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.ghInternship.GHInternship2026.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World!";
+    }
+}
