b835f5ebfbc405afb6065a2400d906320cbcfe04
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2026-09-23 22:59:36 +0600

    paket

--- src/main/java/com/ghInternship/GHInternship2026/controller/HelloController.java
+++ src/main/java/com/ghInternship/GHInternship2026/controller/HelloController.java
@@ -1,5 +1,12 @@
 package com.ghInternship.GHInternship2026.controller;
 
+@SpringBootApplication
 public class HelloController {
+    public class static void main(String[] args){
+
+        
+    
+        
+    }
 
 }
